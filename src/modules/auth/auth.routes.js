59720de529730ed74b7fb8ecc71f8989cec1012d
CarlosDcastano <carlosdueiner1996@gmail.com>
2026-02-20 21:34:48 -0500

    Probando registro y login

--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -5,6 +5,6 @@ const router = Router();
 
 router.post('/register/candidate', authController.registerCandidate);
 router.post('/register/company', authController.registerCompany);
-router.post('/login', authController.login);
+router.post('/login', authController.login);    
 
 export default router;
